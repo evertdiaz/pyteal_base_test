6024eba6c7c726b1580381fc5a36cd285f6cddb2
Evert Diaz <evertdiazb@gmail.com>
2022-09-15 11:16:59 -0500

    base

---
+++ contract.py
@@ -0,0 +1,26 @@
+from pyteal import *
+from pyteal.ast.bytes import Bytes
+
+# Declare application state storage (immutable)
+global_registrar = Bytes("registrar")
+local_diploma = Bytes("diploma")
+
+def approval():
+  return Return(Int(1))
+  
+
+
+def clear():
+  return Return(
+    Int(1)
+  )
+  
+if __name__ == '__main__':
+  import os
+
+  path = os.path.dirname(os.path.abspath(__file__))
+
+  with open(os.path.join(path, "approval.teal"), "w") as f:
+    f.write(compileTeal(approval(), mode=Mode.Application, version=6))
+  with open(os.path.join(path, "clear.teal"), "w") as f:
+    f.write(compileTeal(clear(), mode=Mode.Application, version=6))
